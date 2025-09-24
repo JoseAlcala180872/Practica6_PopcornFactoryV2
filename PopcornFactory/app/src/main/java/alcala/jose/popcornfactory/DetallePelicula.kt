edbd4cb344a8d3923914b3089ca19efac72c1027
JoseAlcala180872 <jose.alcala@potros.itson.edu.mx>
2025-09-24 08:13:17 -0700

    Funcionalidad de asientos para películas.

--- PopcornFactory/app/src/main/java/alcala/jose/popcornfactory/DetallePelicula.kt
+++ PopcornFactory/app/src/main/java/alcala/jose/popcornfactory/DetallePelicula.kt
@@ -1,6 +1,8 @@
 package alcala.jose.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -26,17 +28,41 @@ class DetallePelicula : AppCompatActivity() {
         val nombre: TextView =findViewById(R.id.tv_nombre_pelicula)
         //sinopsis
         val sinopsis:TextView=findViewById(R.id.tv_pelicula_desc)
-
+        //seats
+        val seats: TextView=findViewById(R.id.seatsLeft)
+        //boton
+        val buyTickets: Button =findViewById(R.id.buyTickets)
 
         val bundle = intent.extras
+        var seatsLeft=0
+        var id=-1
+        var title=""
+
         if (bundle != null) {
             val imagenId = bundle.getInt("header")
             val nombrePelicula = bundle.getString("titulo")
             val sinopsisPelicula = bundle.getString("sinopsis")
+            seatsLeft=bundle.getInt("numberSeats")
+            title=bundle.getString("titulo")!!
 
             header.setImageResource(imagenId)
             nombre.text = nombrePelicula
             sinopsis.text = sinopsisPelicula
+            seats.text=("$seatsLeft seats available")
+            id=bundle.getInt("pos")
+        }
+        if (seatsLeft==0){
+            buyTickets.isEnabled=false
+        }
+        else{
+            buyTickets.setOnClickListener {
+                val intent: Intent =Intent(this, SeatSelection::class.java)
+
+                intent.putExtra("id", id)
+                intent.putExtra("name", title)
+
+                this.startActivity(intent)
+            }
         }
     }
 }
